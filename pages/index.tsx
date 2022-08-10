a9fcd45a79a533d68fcc1489db552dc2e4ff5597
MaquinoBSC <martinare.ipn@gmail.com>
2022-08-10 13:51:28 -0500

    Instalacion y setup de NextUI en nuestra app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@nextui-org/react';
 import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
     <>
       <h1>Iniciamos</h1>
+      <Button color={'gradient'}>NextUI</Button>
     </>
   )
 }
