72fe371800eb65ed7ca6491e9abcf031ddeb2fd7
MaquinoBSC <martinare.ipn@gmail.com>
2022-08-10 20:30:26 -0500

    Implementacion de Layout principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import { Button } from '@nextui-org/react';
 import type { NextPage } from 'next';
+import { Layout } from '../components/layouts';
+
 
 const HomePage: NextPage = () => {
   return (
-    <>
-      <h1>Iniciamos</h1>
+    <Layout title='Listado de Pokemones'>
       <Button color={'gradient'}>NextUI</Button>
-    </>
+    </Layout>
   )
 }
 
