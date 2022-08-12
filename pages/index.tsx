9db3f0960c0a01f4e7aec6adf3251b561c40299f
MaquinoBSC <martinare.ipn@gmail.com>
2022-08-12 17:45:48 -0500

    Se obtiene el id y la url de la imagen de cada pokemon y se renderiza de lado del front

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,29 @@
 import type { NextPage, GetStaticProps } from 'next';
 import { pokeApi } from '../api';
 import { Layout } from '../components/layouts';
-import { PokemonListResponse } from '../interfaces/pokemon-list';
+import { PokemonListResponse, SmallPokemon } from '../interfaces/pokemon-list';
 
 
-const HomePage: NextPage = (props) => {
-  console.log(props);
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const HomePage: NextPage <Props> = ({ pokemons }) => {
+  console.log(pokemons);
 
   return (
     <Layout title='Listado de Pokemones'>
       <ul>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
+        {
+          pokemons.map((pokemon) => (
+            <div>
+              <p>{ pokemon.id }</p>
+              <p>{ pokemon.name }</p>
+              <img src={ pokemon.img } alt="" />
+              
+            </div>
+          ))
+        }
       </ul>
     </Layout>
   )
@@ -30,10 +33,19 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   const { data } = await pokeApi.get <PokemonListResponse>('/pokemon?limit=151');  
 
 
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, idx) => {
+    return {
+      name: pokemon.name,
+      url: pokemon.url,
+      id: `${idx + 1}`,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${idx+1}.svg`
+    }
+  })
 
+  // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg
   return {
     props: {
-      pokemons: data.results
+      pokemons,
     }
   }
 }
