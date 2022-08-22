98f981cef55bc25d9feed6a029112eee3598a7d8
MaquinoBSC <martinare.ipn@gmail.com>
2022-08-21 21:52:59 -0500

    Se hace la navegación de un pokemon en favoritos a la ruta dinamica del pokemon

--- components/pokemon/FavoriteCardPokemon.tsx
+++ components/pokemon/FavoriteCardPokemon.tsx
@@ -1,16 +1,22 @@
 import { FC } from "react";
-
+import { useRouter } from 'next/router';
 import { Card, Grid } from "@nextui-org/react";
-
 
 interface Props {
     pokemonId: number;
 }
 
+
 const FavoriteCardPokemon: FC <Props> = ({ pokemonId }) => {
+    const router = useRouter();
+
+    const onPokemonClicked = (pokemonId : number) => {
+        router.push(`/pokemon/${pokemonId}`)
+    }
+
     return (
         <Grid xs={ 6 } sm={ 3 } md={ 2 } xl={ 1 } key={ pokemonId }>
-            <Card hoverable clickable css={{ padding: 10 }}>
+            <Card hoverable clickable css={{ padding: 10 }} onClick={ ()=> onPokemonClicked(pokemonId) }>
                 <Card.Image
                     src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/'+pokemonId+'.svg'}
                     width={'100%'}
