cd67af5e5c7df45448b071228232b1b88ec418a4
MaquinoBSC <martinare.ipn@gmail.com>
2022-08-24 21:04:00 -0500

    Open Grapgh Meta Tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,11 +1,14 @@
-import { FC, PropsWithChildren } from "react";
+import { FC } from "react";
 import Head from "next/head";
+
 import { NavBar } from '../UI';
 
 interface Props {
     children: JSX.Element;
     title?: string;
 }
+
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
 
 export const Layout: FC <Props> = ({ children, title }) => {
     return (
@@ -15,6 +18,10 @@ export const Layout: FC <Props> = ({ children, title }) => {
                 <meta name="author" content="Maquino Hernandez" />
                 <meta name="description" content={ `Información sobre algun pokemon ${title}`} />
                 <meta name="keywords" content={ `${title}, pokemon, pokedex` } />
+                
+                <meta property="og:title" content={`Información sobre el pokemon ${title}`} />
+                <meta property="og:description" content={`Esta es la pagina sobre ${title}`} />
+                <meta property="og:image" content={`${origin}/img/banner.png`} />
             </Head>
 
             <NavBar />
