56bb7f8ee33ce1c96366051e269affd454043f05
MaquinoBSC <martinare.ipn@gmail.com>
2022-08-25 22:55:33 -0500

    Se agrega configuración para activar el Incremental Static Generation en pokemons por Name

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -116,16 +116,27 @@ export const getStaticPaths: GetStaticPaths = async(ctx) => {
     
     return {
         paths: pokemonsByNamePath,
-        fallback: false,
+        fallback: 'blocking',
     }
 }
 
 export const getStaticProps: GetStaticProps = async(ctx) => {
     const { name } = ctx.params as { name: string }
+
+    const pokemon = await getPokemonInfo(name);
+
+    if(!pokemon){
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
     
     return {
         props: {
-            pokemon: await getPokemonInfo(name)
+            pokemon
         }
     }
 }
